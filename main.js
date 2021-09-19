e3d371338dbf4afe626030cc65d82c4fbd382a1e
tokq <melindachang.hy@gmail.com>
2021-09-18 23:11:19 -0700

    widepeeposad

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ t1.to('.nav-container', 1, {
   left: 0,
   ease: 'Expo.easeInOut',
 });
-
 t1.staggerFrom('.menu > div', 0.8, { y: 100, opacity: 0, ease: 'Expo.easeOut' }, '0.1', '-=0.4');
 
 t1.staggerFrom('.socials', 0.8, { y: 100, opacity: 0, ease: 'Expo.easeOut' }, '0.4', '-=0.6');
