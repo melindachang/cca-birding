0b4df14c5b829cd2263f884f52db5b2c096b7c91
Aiden Bai <aiden.bai05@gmail.com>
2021-09-18 23:42:24 -0700

    ACCEPT JESUS CHRIST INTO YOUR LIFE

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import './style.sass';
 import gsap from 'gsap';
 
-var t1 = new gsap.timeline({ paused: true });
+const t1 = new gsap.timeline({ paused: true });
 
 t1.to('.nav-container', 1, {
   left: 0,
@@ -12,9 +12,9 @@ t1.staggerFrom('.menu > div', 0.8, { y: 100, opacity: 0, ease: 'Expo.easeOut' },
 t1.staggerFrom('.socials', 0.8, { y: 100, opacity: 0, ease: 'Expo.easeOut' }, '0.4', '-=0.6');
 
 t1.reverse();
-document.querySelector('.menu-open').addEventListener('click', function () {
+document.querySelector('.menu-open').addEventListener('click', () => {
   t1.reversed(!t1.reversed());
 });
-document.querySelector('.menu-close').addEventListener('click', function () {
+document.querySelector('.menu-close').addEventListener('click', () => {
   t1.reversed(!t1.reversed());
 });
